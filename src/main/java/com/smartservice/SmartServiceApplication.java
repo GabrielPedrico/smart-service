5517156e4e3633fce543a0548f6a23f212fb31a7
gabrielpedrico <gabriel.pedrico@fatec.sp.gov.br>
2021-11-24 13:05:28 -0300

    Spring bot + dependencias adicionados.

---
+++ src/main/java/com/smartservice/SmartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartServiceApplication.class, args);
+	}
+
+}
