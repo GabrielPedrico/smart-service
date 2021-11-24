efad58e8021de6d2766c2961cecd0a17e5cc543d
gabrielpedrico <gabriel.pedrico@fatec.sp.gov.br>
2021-11-24 18:22:57 -0300

    Liberando end-point teste para o front.

--- src/main/java/com/smartservice/adapter/http/spring/TesteController.java
+++ src/main/java/com/smartservice/adapter/http/spring/TesteController.java
@@ -1,6 +1,7 @@
 package com.smartservice.adapter.http.spring;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TesteController {
 
     @PostMapping
+    @CrossOrigin
     ResponseEntity<?> postHelloWorld(){
         return ResponseEntity.ok().body("Hello world");
     }
