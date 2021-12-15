bd7f89d4a85ae5ec500a083822901296f6b7d8fc
Gabriel Pedrico <gabriel.pedrico@fatec.sp.gov.br>
2021-12-15 16:20:51 -0300

    fix-arq-hexagonal

---
+++ src/main/java/com/smartservice/config/annotations/AdapterUseCase.java
@@ -0,0 +1,12 @@
+package com.smartservice.config.annotations;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface AdapterUseCase {
+}
