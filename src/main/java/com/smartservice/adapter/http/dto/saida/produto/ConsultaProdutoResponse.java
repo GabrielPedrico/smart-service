4d7235915ef2b6247574fa12ec8b70759ad44565
Gabriel Pedrico <gabriel.pedrico@fatec.sp.gov.br>
2022-05-18 15:21:36 -0300

    -Novo end-point que para resgatar categorias. -Correcao retorno produtos campo "categoria".

--- src/main/java/com/smartservice/adapter/http/dto/saida/produto/ConsultaProdutoResponse.java
+++ src/main/java/com/smartservice/adapter/http/dto/saida/produto/ConsultaProdutoResponse.java
@@ -1,21 +1,63 @@
 package com.smartservice.adapter.http.dto.saida.produto;
 
-import com.smartservice.adapter.datastore.entities.Produto;
-
-import java.util.List;
+import java.math.BigDecimal;
 
 public class ConsultaProdutoResponse {
 
-    private List<Produto> produtos;
+    private String id;
 
-    public ConsultaProdutoResponse(List<Produto> produtos) {
-        this.produtos = produtos;
+    private String categoria;
+
+    private String nome;
+
+    private BigDecimal preco;
+
+    private String descricao;
+
+    private Integer estoque;
+
+    private String imgUrl;
+
+    public ConsultaProdutoResponse(String id, String categoria, String nome, BigDecimal preco, String descricao, Integer estoque, String imgUrl) {
+        this.id = id;
+        this.categoria = categoria;
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+        this.estoque = estoque;
+        this.imgUrl = imgUrl;
     }
 
+    @Deprecated
     public ConsultaProdutoResponse() {
     }
 
-    public List<Produto> getProdutos() {
-        return produtos;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
     }
 }
