69bb0fa8de8431ab9c6b9e1eb3c62385bbe9c2d9
Gabriel Pedrico <gabriel.pedrico@fatec.sp.gov.br>
2022-05-21 10:21:08 -0300

    cors-liberado para alguns end-points

--- src/main/java/com/smartservice/adapter/http/spring/controllers/mesa/ConsultaMesaController.java
+++ src/main/java/com/smartservice/adapter/http/spring/controllers/mesa/ConsultaMesaController.java
@@ -6,6 +6,7 @@ import com.smartservice.adapter.http.spring.dto.saida.mesa.ConsultaMesaResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class ConsultaMesaController {
     MesaService mesaService;
 
     @GetMapping("/mesa/{id}")
+    @CrossOrigin
     public ResponseEntity<?> consultaMesa(@PathVariable("id") String id){
         ConsultaMesaResponse response = mesaService.consultaMesaPort().consultaMesa(id);
         return getResponseData(buildResponseData(response), HttpStatus.OK);
